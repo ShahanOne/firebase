1f1b66e7941f438755a4e5c79e75b3c168c2188e
Syed Shahan Hussain <shahanhussain9105@gmail.com>
2024-01-04 03:53:50 +0530

    env

--- config/firebase.js
+++ config/firebase.js
@@ -4,18 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD5_aYZ_hahAMFh_gn284CtK5xcESAns7g',
-  authDomain: 'random-25c22.firebaseapp.com',
-  projectId: 'random-25c22',
-  storageBucket: 'random-25c22.appspot.com',
-  messagingSenderId: '53043704080',
-  appId: '1:53043704080:web:a5bc293c74ea0b9ff97595',
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
